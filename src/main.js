0337b65c97b30f0455400d0f4eff17ae46328646
ssh68 <610353134@qq.com>
2019-04-22 11:34:46 +0800

    登录功能开发完毕

--- src/main.js
+++ src/main.js
@@ -5,8 +5,21 @@ import router from './router'
 //  引入全局控制css样式文件
 import './assets/css/global.css'
 
+// 引入icon图标库样式文件
+import './assets/fonts/iconfont.css'
+
+// 引入axios
+import axios from 'axios'
+
+// 引入elementui组件库
+import ElementUI from 'element-ui'
+// 注册组件
+Vue.use(ElementUI)
 // Vue.config.productionTip = false
 
+// 给axios做配置
+axios.defaults.baseURL = 'http://127.0.0.1:11333/api/private/v1/'
+Vue.prototype.$http = axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
